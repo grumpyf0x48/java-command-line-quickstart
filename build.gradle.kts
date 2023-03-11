aa70d205a3055a5b175d1c38ee6b175ad2dbc738
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2023-03-11 18:04:37 +0100

    Add targetCompatibility version

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,10 @@ application {
     mainClass.set("org.grumpyf0x48.applicationName.Command")
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_17
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
